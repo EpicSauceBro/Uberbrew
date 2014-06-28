e0e1ace69923056ab13fb64e1d6959154d9d34a4
EpicSauceBro <a.yasariz@gmail.com>
2014-06-28 09:37:49 -0700

    Moved stuff.

--- src/java/me/AliSwag/Main.java
+++ src/java/me/AliSwag/Main.java
@@ -2,19 +2,34 @@ package me.AliSwag;
 
 import me.AliSwag.commands.DrinkCommand;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
-	
+
+    static Main main;
+
 	@Override
 	public void onEnable(){
-		
+		main = this;
+
 		getCommand("uberbrew").setExecutor(new DrinkCommand());
-		
 	}
 	
 	@Override
 	public void onDisable(){
 	}
+
+    public static FileConfiguration getMainConfig() {
+        return main.getConfig();
+    }
+
+    public static void saveMainConfig() {
+        main.saveConfig();
+    }
+
+    public static void reloadMainConfig() {
+        main.reloadConfig();
+    }
 
 }
