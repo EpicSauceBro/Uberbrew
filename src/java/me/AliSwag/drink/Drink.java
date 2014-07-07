2d839161096bcb678a4f4c758476f3e87a4d65cf
Searchndstroy <s3archndstroy@gmail.com>
2014-07-06 22:14:01 -0700

    Edited one whole line

--- src/java/me/AliSwag/drink/Drink.java
+++ src/java/me/AliSwag/drink/Drink.java
@@ -1,11 +1,11 @@
 package me.AliSwag.drink;
 
 public class Drink {
-	
+
 	public int alcoholContent;
 	public String displayName;
+	public String name;
 
-    public String name;
 	public Drink(int percent, String displayName, String name) {
 		this.alcoholContent = percent;
 		this.displayName = displayName;
@@ -15,9 +15,9 @@ public class Drink {
     public int getAlcoholContent() { return this.alcoholContent; }
     public String getName () {return this.name; }
     public String getDisplayName() { return this.displayName; }
-    
     @Override
     public String toString() {
         return name + ":" + displayName + ":" + alcoholContent;
     }
+
 }
