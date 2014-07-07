0068d450748dc305de29c32113a2206fec0c238c
Searchndstroy <s3archndstroy@gmail.com>
2014-07-06 21:13:40 -0700

    Added toString function to Drink.java

--- src/java/me/AliSwag/drink/Drink.java
+++ src/java/me/AliSwag/drink/Drink.java
@@ -4,8 +4,8 @@ public class Drink {
 	
 	public int alcoholContent;
 	public String displayName;
-	public String name;
-	
+
+    public String name;
 	public Drink(int percent, String displayName, String name) {
 		this.alcoholContent = percent;
 		this.displayName = displayName;
@@ -15,5 +15,9 @@ public class Drink {
     public int getAlcoholContent() { return this.alcoholContent; }
     public String getName () {return this.name; }
     public String getDisplayName() { return this.displayName; }
-
+    
+    @Override
+    public String toString() {
+        return name + ":" + displayName + ":" + alcoholContent;
+    }
 }
